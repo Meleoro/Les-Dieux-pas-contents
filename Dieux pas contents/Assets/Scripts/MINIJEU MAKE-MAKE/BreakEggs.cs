0660aad6363fae43849634892ea1634e865bbdc3
5howdown <o.reynoud@rubika-edu.com>
2022-09-06 23:51:45 +0200

    oui

--- Dieux pas contents/Assets/Scripts/MINIJEU MAKE-MAKE/BreakEggs.cs
+++ Dieux pas contents/Assets/Scripts/MINIJEU MAKE-MAKE/BreakEggs.cs
@@ -2,26 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BreakEggs : MonoBehaviour
 {
+    [SerializeField] private CircleCollider2D collider2D;
     [SerializeField] private GameObject haut;
     [SerializeField] private GameObject bas;
-
     [SerializeField] private GameObject jaune;
     [SerializeField] private Rigidbody2D hautRb;
     [SerializeField] private Rigidbody2D basRb;
     [SerializeField] private Rigidbody2D jauneRb;
     [SerializeField] private Rigidbody2D eggRb;
-    [SerializeField] private Vector3 oeufPos;
+    public OeufManager oeufManager;
 
     [SerializeField] private Vector3 morceauxPos;
     [SerializeField] private float breakPoint = 100;
 
-    public float speed = 1000;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(EggLaunch());
+        oeufManager = GameObject.Find("PANIER").GetComponent<OeufManager>();
     }
 
     // Update is called once per frame
@@ -41,6 +44,7 @@ public class BreakEggs : MonoBehaviour
             jaune.SetActive(true);
             InheritVelocity(jauneRb);
             gameObject.SetActive(false);
+            oeufManager.EggRespawn();
         }
     }
 
@@ -50,6 +54,16 @@ public class BreakEggs : MonoBehaviour
         Vector3 dir = morceauxPos - gameObject.transform.position;
         Vector3 normDir = dir.normalized;
         morceauxDoeuf.AddForce(normDir * eggRb.velocity * speed * -1, ForceMode2D.Impulse);
+    }
+
+    public IEnumerator EggLaunch()
+    {
+        eggRb.AddForce(Vector2.up * (15), ForceMode2D.Impulse);
+        eggRb.angularVelocity = Random.Range(-500, 501);
+        yield return new WaitForSeconds(0.5f);
+        collider2D.enabled = true;
+        gameObject.GetComponent<CircleCollider2D>().enabled = true;
+
     }
 }
 
