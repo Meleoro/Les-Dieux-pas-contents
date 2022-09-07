af8e49eee00b626ba64dd5747a705dbedf060f9a
5howdown <o.reynoud@rubika-edu.com>
2022-09-07 12:00:16 +0200

    oui oui vous avez raison!

--- Dieux pas contents/Assets/Scripts/MINIJEU ZEUS/SpawnIngredients.cs
+++ Dieux pas contents/Assets/Scripts/MINIJEU ZEUS/SpawnIngredients.cs
@@ -23,9 +23,12 @@ public class SpawnIngredients : MonoBehaviour
     public GameOver loseCondition;
 
     [SerializeField] private float startDelay;
+    [SerializeField] private bool ange2 = false;
 
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private TextMeshProUGUI texte;
+
+    private bool canDelay;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,8 +36,10 @@ public class SpawnIngredients : MonoBehaviour
         loseCondition = GameObject.Find("MiniManager").GetComponent<GameOver>();
         startDelay = baseDelay;
         score = 0;
-        Time.timeScale = 0;
-        Ange.Instance.AngeApparait("OK nous y voila", 1, "Pour ta première mission comme Zeus te l'a dit est de préparer à manger", 2, "Pour cela rien de plus simple il suffit de récuperer les bons ingrédients dans la casserole",3,"Pour bouger la casserole il te faut simplement bouger la souris de gauche à droite de facon à ce que la casserole soit sous des pates' un steak ou bien une tomate.",3,"Mais SURTOUT PAS un sac poubelle ou une arrete de poisson. Sur ceux bonne chance élu de la prophétie!", 2);
+        canDelay = false;
+        StartCoroutine(StartAnge());
+        Ange.Instance.AngeApparait("OK nous y voila", 3, "Pour ta première mission comme Zeus te l'a dit est de préparer à manger", 4, "Pour cela rien de plus simple il suffit de récuperer les bons ingrédients dans la casserole",8,"Pour bouger la casserole il te faut simplement bouger la souris de gauche à droite de facon à ce que la casserole soit sous des pates' un steak ou bien une tomate.",6,"Mais SURTOUT PAS un sac poubelle ou une arrete de poisson. Sur ceux bonne chance élu de la prophétie!", 6);
+        
     }
 
     // Update is called once per frame
@@ -42,11 +47,11 @@ public class SpawnIngredients : MonoBehaviour
     {
         texte.text = badScore + "/10";
         text.text = score + "/5";
-        if (baseDelay >= 0)
+        if (baseDelay >= 0 && canDelay)
         {
             baseDelay -= Time.deltaTime;
         }
-        else
+        else if(canDelay)
         {
             baseDelay = startDelay;
             spawnX = Random.Range(-10, 11);
@@ -59,9 +64,10 @@ public class SpawnIngredients : MonoBehaviour
             loseCondition.Lost = true;
         }
 
-        if (score == 2)
+        if (score == 2 && !ange2)
         {
-
+            ange2 = true;
+            
         }
 
 
@@ -79,6 +85,12 @@ public class SpawnIngredients : MonoBehaviour
         {
             Destroy(other.gameObject);
         }
+    }
+
+    private IEnumerator StartAnge()
+    {
+        yield return new WaitForSeconds(27);
+        canDelay = true;
     }
     
 }
